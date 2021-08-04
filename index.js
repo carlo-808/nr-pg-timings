3e9e9b5cbd122807fa1593d9869fd1bba7da3cf5
Carlo Pearson <cpearson@newrelic.com>
2021-08-04 16:16:53 -0700

    add more routes/cases

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict"
 
 const newrelic = require("newrelic")
-const { Client } = require("pg")
+const { Client, Query } = require("pg")
 const Koa = require('koa')
 const Router = require('koa-router')
 const dbconfig = require('./dbconfig')
@@ -18,14 +18,21 @@ const asyncData = async () => {
   } catch (err) {
     console.error("Unable to connect to the database:", err)
   }
-  const results = await client.query(`SELECT pg_sleep(1), now() as sleep`)
+
+  let results
+
+  try {
+    results = await client.query(`SELECT pg_sleep(2), now() as sleep`)
+  } catch (err) {
+    console.error(err)
+  }
   let result = ''
-  if (results.rows.length > 0) {
+  if (results.rows?.length > 0) {
     result = results.rows[0].sleep
   }
 
   await client.end()
-
+  console.log(result)
   return result
 }
 
@@ -39,7 +46,7 @@ const syncData = () => {
         reject(err)
       }
 
-      client.query(`SELECT pg_sleep(1), now() as sleep`,
+      client.query(`SELECT pg_sleep(2), now() as sleep`,
         (err, result) => {
           if (err) {
             client.end()
@@ -49,6 +56,37 @@ const syncData = () => {
           client.end()
           resolve(result)
         })
+    })
+  })
+}
+
+const submitStyleData = () => {
+  const client = new Client(dbconfig)
+
+  return new Promise((resolve, reject) => {
+    client.connect((err) => {
+      if (err) {
+        client.end()
+        reject(err)
+      }
+
+      let data = []
+
+      const query = client.query(new Query(`SELECT pg_sleep(2), now() as sleep`))
+
+      query.on('error', () => {
+        client.end()
+        reject(err)
+      })
+
+      query.on('row', (row) => {
+        data.push(row)
+      })
+
+      query.on('end', () => {
+        resolve(data)
+        client.end()
+      })
     })
   })
 }
@@ -65,6 +103,10 @@ router.get('/', async (ctx, next) => {
   const result = await syncData()
   ctx.body = result
 })
+.get('/submitStyle', async (ctx) => {
+  const result = await submitStyleData()
+  ctx.body = JSON.stringify(result)
+})
 .get('/kill', async (ctx) => {
   ctx.body = 'goodbye'
   process.exit(0)
@@ -73,6 +115,18 @@ router.get('/', async (ctx, next) => {
 app.use(router.routes())
 app.use(router.allowedMethods())
 
+process.on('uncaughtException', () => {
+  newrelic.shutdown({
+    collectPendingData: true
+  }, () => {
+    process.exit()
+  })
+})
+
 app.listen(PORT, () => {
   console.log(`app listening at port: localhost:${PORT}`)
+
+  // setTimeout(() => {
+  //   dog = cat
+  // }, 70000)
 })
